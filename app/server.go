24624630a4a21dc3400bad0899a9c8bc049d6fa5
leotao <leotao@umich.edu>
2024-06-22 04:22:18 -0400

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -1,40 +1,59 @@
 package main
-
 import (
 	"fmt"
 	"net"
 	"os"
 	"strings"
 )
-
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	//binding to port
+	//bind to port 4221
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
-	   fmt.Println("Failed to bind to port 4221")
-	   os.Exit(1)
+		fmt.Println("Failed to bind to port 4221")
+		os.Exit(1)
 	}
-	//start to accepting connections
-	conn, err := l.Accept()
+	defer l.Close() //close when everything finished
+
+	//accept connection
+	for {
+		connection, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			continue
+		}
+		go handleConnection(connection)
+	}
+}
+func handleConnection(connection net.Conn) {
+	defer connection.Close() //close when everything finished
+
+	//read conn into buffer
+	requestBuffer := make([]byte, 1024)
+	n, err := connection.Read(requestBuffer)
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-	 	os.Exit(1)
+		fmt.Println("Failed to read the request:", err)
+		return
 	}
 
-	//read connection into byte
-	req := make([]byte, 1024)
-	conn.Read(req)
+	//console testing
+	fmt.Printf("Request: %s\n", requestBuffer[:n])
 
-	path := strings.Split(conn, " ")[1]
+	request := string(requestBuffer[:n])
+
+	//retrieve url path
+	path := strings.Split(request, " ")[1]
+
+	//default 200OK
 	if path == "/" {
 		connection.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-	} else if strings.Split(path, "/")[1] == "echo" {
+	}
+	//echo request
+	else if strings.Split(path, "/")[1] == "echo" {
 		message := strings.Split(path, "/")[2]
 		connection.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(message), message)))
-	} else {
+	}
+	//invalid 404
+	else {
 		connection.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 	}
 }
