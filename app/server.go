8f79ca546c8a23d0b482ab928c57ea19a5681d5d
leotao <leotao@umich.edu>
2024-06-23 00:56:38 -0400

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -13,7 +13,6 @@ func main() {
 		os.Exit(1)
 	}
 	defer l.Close() //close when everything finished
-
 	//accept connection
 	for {
 		connection, err := l.Accept()
@@ -26,7 +25,6 @@ func main() {
 }
 func handleConnection(connection net.Conn) {
 	defer connection.Close() //close when everything finished
-
 	//read conn into buffer
 	requestBuffer := make([]byte, 1024)
 	n, err := connection.Read(requestBuffer)
@@ -34,31 +32,31 @@ func handleConnection(connection net.Conn) {
 		fmt.Println("Failed to read the request:", err)
 		return
 	}
-
 	//console testing
 	fmt.Printf("Request: %s\n", requestBuffer[:n])
-
 	request := string(requestBuffer[:n])
-
-	method := strings.Split(request, " ")[0]
+	//retrieve url path
+	meth := strings.Split(request," ")[0]
 	path := strings.Split(request, " ")[1]
-
 	response := ""
-
 	if path == "/" {
 		//default 200OK
+		connection.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 		response = "HTTP/1.1 200 OK\r\n\r\n"
 	} else if strings.Split(path, "/")[1] == "echo" {
 		//echo request
 		message := strings.Split(path, "/")[2]
+		connection.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(message), message)))
 		response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(message), message)
-	}else if strings.Split(path, "/")[1] == "user-agent"{
+	} else if strings.Split(path, "/")[1] == "user-agent" {
 		//user-agent
 		temp := strings.Split(request, ":")[3]
-		message := strings.Split(temp,"\r\n")[0]
+		message := strings.Split(temp, "\r\n")[0]
+		message = strings.ReplaceAll(message, " ", "")
+		connection.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(message), message)))
 		message = strings.ReplaceAll(message, " ", "") //clean whitespace
 		response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(message), message)
-	}else if strings.Split(path, "/")[1] == "files" && method == "GET"{
+	} else if strings.Split(path, "/")[1] == "files" {
 		//files
 		dir := os.Args[2]
 		fileName := strings.TrimPrefix(path, "/files/")
@@ -68,17 +66,10 @@ func handleConnection(connection net.Conn) {
 		} else {
 			response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%s", len(data), data)
 		}
-	}else if strings.Split(path, "/")[1] == "files" && method == "POST"{
-		content:= strings.Split(request,"\r\n")[len(request)-1]
-		content = strings.Trim(content , "\x00")
-		dir := os.Args[2]
-		_ = os.WriteFile((dir + path[7:]), []byte(content), 0644)
-		response = "HTTP/1.1 201 Created\r\n\r\n"
-
-	}else {
+	} else {
 		//invalid 404
+		connection.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 		response = "HTTP/1.1 404 Not Found\r\n\r\n"
 	}
-
 	connection.Write([]byte(response))
 }
