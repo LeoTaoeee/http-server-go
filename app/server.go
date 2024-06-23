c3ef8835a678b302d59428f5f7644ad1e803a656
leotao <leotao@umich.edu>
2024-06-23 01:04:46 -0400

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -74,18 +74,39 @@ func handleConnection(connection net.Conn) {
 		}
 	}else if meth == "POST"{
 		if strings.Split(path, "/")[1] == "files" {
-			//files
-			dir := os.Args[2]
-			fileName := strings.TrimPrefix(path, "/files/")
-			data, err := os.ReadFile(dir + fileName)
-			if err != nil {
-				response = "HTTP/1.1 404 Not Found\r\n\r\n"
-			} else {
-				response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%s", len(data), data)
+			// Parse headers to get Content-Length
+			headers := strings.Split(request, "\r\n")
+			contentLength := 0
+			for _, header := range headers {
+				if strings.HasPrefix(header, "Content-Length:") {
+					lengthStr := strings.TrimSpace(strings.TrimPrefix(header, "Content-Length:"))
+					contentLength, err = strconv.Atoi(lengthStr)
+					if err != nil {
+						fmt.Println("Failed to parse Content-Length:", err)
+						response = "HTTP/1.1 400 Bad Request\r\n\r\n"
+						break
+					}
+				}
+			}
+
+			// Read the request body
+			if contentLength > 0 {
+				bodyStart := strings.Index(request, "\r\n\r\n") + 4
+				body := requestBuffer[bodyStart : bodyStart+contentLength]
+				fileName := strings.TrimPrefix(path, "/files/")
+				dir := os.Args[2]
+
+				// Write the request body to the file
+				err = os.WriteFile(dir+fileName, body, 0644)
+				if err != nil {
+					fmt.Println("Failed to write the file:", err)
+					response = "HTTP/1.1 500 Internal Server Error\r\n\r\n"
+				} else {
+					response = "HTTP/1.1 201 Created\r\n\r\n"
+				}
 			}
 		} else {
-			//invalid 404
-			connection.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+			// Invalid 404
 			response = "HTTP/1.1 404 Not Found\r\n\r\n"
 		}
 	}
